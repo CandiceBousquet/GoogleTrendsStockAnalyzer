28f05cc3537cc98028363883abc40475cc2c81f1
Candice Bousquet <candice@havenlife.com>
2022-10-05 22:30:47 -0400

    use dynamic port for heroku

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors_proxy = require('cors-anywhere');
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use('/public', express.static(path.join(__dirname, '../public')));
 app.use('/api', require('./api'));
@@ -15,16 +15,3 @@ app.use('/', (req, res, next) => {
 app.listen(PORT, () => {
 	console.log(`server listening on port ${PORT}`);
 });
-
-console.log('adfasf');
-
-// const HOST = process.env.HOST || '0.0.0.0';
-// const proxy = 8888;
-// cors_proxy.createServer({
-//     originWhitelist: [], // Allow all origins
-//     // requireHeader: ['origin', 'x-requested-with'],
-//     requireHeader: ['origin', 'x-requested-with'],
-//     // removeHeaders: ['cookie', 'cookie2']
-// }).listen(proxy, HOST, function() {
-//     console.log('Running CORS Anywhere on ' + HOST + ':' + proxy);
-// });
